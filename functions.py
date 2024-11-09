dee7e3a8a5f712bd4ad5b5cb5495d994e8876ba8
KDoyle06 <kdoyle04@iastate.edu>
2024-11-09 12:21:16 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -111,5 +111,12 @@ def calcRoadLoad(rollres,weight,tslope,airden,dragC,csA,v):
     airres=.5*airden*dragC*csA*v**2
     rRoll=rollres*weight*np.cos((np.atan(tslope/100)))
     roadLoad=rRoll+weight*np.sin((np.atan(tslope/100)))+airres
+    
+def torquestar(torque,angularve,gears):
+    torquet=torque*gears
+    angularvet=angularve*gears
+    return torquet,angularvet
+    
+    
 
 
